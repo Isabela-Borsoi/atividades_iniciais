62ae74f376a08fe82818b4edb2fd633ec49c2762
Isabela Cassol <isabela_borsoi@estudante.sesisenai.org.br>
2024-06-26 21:18:31 -0300

    exercício 05

---
+++ LISTA-01/exercício_05/index.js
@@ -0,0 +1,10 @@
+let esconder = document.getElementById('esconder')
+
+esconder.addEventListener('click', function() {
+    document.getElementById("mensagem").style.display = "none";
+})
+let mostrar = document.getElementById('mostrar')
+
+mostrar.addEventListener('click', function () {
+    document.getElementById("mensagem").style.display = "block";
+})
