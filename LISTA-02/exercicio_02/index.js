40d3027447f39c236e8cc4e1a4e99c04d4f3df25
Isabela Cassol <isabela_borsoi@estudante.sesisenai.org.br>
2024-07-03 19:40:03 -0300

    EXERCÍCIO 02

---
+++ LISTA-02/exercicio_02/index.js
@@ -0,0 +1,22 @@
+
+const formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    if (!nome){
+        window.alert('preencha o nome')
+        return
+    }
+    if (!email){
+        Window.alert('preencha o email')
+        return
+    }
+    console.log({
+        "nome": nome,
+        "email": email
+    })
+
+})
